c0eea555b1cf374cd7a877c68f054ddeb67e63fe
Aarush-Jain <ajjacky1909@gmail.com>
2020-07-08 16:55:58 +0530

    committed

--- sketch.js
+++ sketch.js
@@ -1,48 +1,25 @@
-var ball;
+var canvas,backgroundImg;
+var gameState = 0;
+var playerCount;
 var database;
-var position;
+var form,player,game;
+var allPlayers;
+var car1,car2,car3,car4,cars;
 
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    canvas = createCanvas(displayWidth-60,displayHeight-60);
     database = firebase.database();
-    var ballPosition = database.ref("ball/position");
-    ballPosition.on("value",readPosition,showError);
+    game = new Game();
+    game.getState();
+    game.start();
 }
 
 function draw(){
-    background("white");
-    if (position!==undefined){
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
+    if(playerCount===4){
+        game.update(1);
     }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
+    if(gameState===1){
+        clear();
+        game.play();
     }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
-}
-}
-
-function writePosition(x,y){
-    database.ref("ball/position").set({
-        'x': position.x + x,
-        'y': position.y + y
-    })
-}
-
-function readPosition(data){
-   position = data.val();
-   ball.x = position.x;
-   ball.y = position.y;
-}
-
-function showError(){
-    console.log("error");
 }
